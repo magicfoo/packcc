04231b054b480edb71a86736b2e9ab55e71de269
magic/foo <ggainant@gmail.com>
2019-05-08 18:49:22 -0500

    Update packcc.cpp

--- packcc.cpp
+++ packcc.cpp
@@ -2,6 +2,7 @@
  * PackCC: a packrat parser generator for C.
  *
  * Copyright (c) 2014 Arihiro Yoshida. All rights reserved.
+ * Copyright (c) 2019 Gerald Gainant. All rights reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
